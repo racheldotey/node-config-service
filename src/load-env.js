bd7ca5773d25ea97f913e43c3c6a7aada851bab8
Rachel Dotey <rachel.dotey@gmail.com>
2022-09-27 15:10:31 -0400

    Update load-env.js
    Auto formatting.

--- src/load-env.js
+++ src/load-env.js
@@ -3,7 +3,7 @@ var wasInitialized;
 
 // Loads environment variables from a .env file into process.env. 
 // https://www.npmjs.com/package/dotenv
-if(!wasInitialized) {
+if (!wasInitialized) {
     console.count('dotenv wasInitialized');
     require('dotenv').config();
     wasInitialized = true;
