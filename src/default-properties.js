5851b43393b866d516a5ebe39bbd3512e40d64b3
Rachel Dotey <rachel.dotey@gmail.com>
2022-09-28 15:11:36 -0400

    Create default-properties.js

---
+++ src/default-properties.js
@@ -0,0 +1,39 @@
+/*!
+ * Default config properties.
+ */
+
+'use strict';
+
+const path = require('path');
+
+
+/**
+ * CONFIG_PROPERTIES
+ * 
+ * @memberOf NodeConfigService
+ * @desc Object defining default config properties.
+ * @constant
+ * @type {Object}
+ * @default
+ */
+const CONFIG_PROPERTIES = Object.freeze({
+    NODE_ENV: {
+        key: 'NODE_ENV',
+        name: 'environment',
+        desc: `{String} Current environment likely 'production' or 'development'.`,
+        default: 'development',
+        required: false,
+        parse: value => value.toString().toLowerCase()
+    },
+    ROOT_DIR: {
+        key: 'ROOT_DIR',
+        name: 'rootDir',
+        desc: `{String} The root directory (absolute system path) for the application.`,
+        default: __dirname,
+        required: true,
+        parse: value => path.normalize(value)
+    }
+});
+
+
+module.exports = CONFIG_PROPERTIES;
