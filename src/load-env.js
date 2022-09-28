9b938e68b17959af0d0f741a243cf5c5a9218529
Rachel Dotey <rachel.dotey@gmail.com>
2022-09-27 23:37:12 -0400

    Update load-env.js

--- src/load-env.js
+++ src/load-env.js
@@ -1,10 +1,7 @@
 // NOTE: Should be placed as early as possible on startup.
-var wasInitialized;
-
 // Loads environment variables from a .env file into process.env. 
 // https://www.npmjs.com/package/dotenv
-if (!wasInitialized) {
-    console.count('dotenv wasInitialized');
-    require('dotenv').config();
-    wasInitialized = true;
-}
+console.count('dotenv wasInitialized');
+require('dotenv').config();
+
+module.exports = process.env;
