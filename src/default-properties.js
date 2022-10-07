38e4871eb05e9b38cfc22ffa5419039e31c0fb9c
Rachel Dotey <rachel.dotey@gmail.com>
2022-10-07 17:38:21 -0400

    Update default-properties.js
    Prettier formatting.

--- src/default-properties.js
+++ src/default-properties.js
@@ -2,14 +2,11 @@
  * Default config properties.
  */
 
-'use strict';
-
 const path = require('path');
-
 
 /**
  * CONFIG_PROPERTIES
- * 
+ *
  * @memberOf NodeConfigService
  * @desc Object defining default config properties.
  * @constant
@@ -17,23 +14,22 @@ const path = require('path');
  * @default
  */
 const CONFIG_PROPERTIES = Object.freeze({
-    NODE_ENV: {
-        key: 'NODE_ENV',
-        name: 'environment',
-        desc: `{String} Current environment likely 'production' or 'development'.`,
-        default: 'development',
-        required: false,
-        parse: value => value.toString().toLowerCase()
-    },
-    ROOT_DIR: {
-        key: 'ROOT_DIR',
-        name: 'rootDir',
-        desc: `{String} The root directory (absolute system path) for the application.`,
-        default: __dirname,
-        required: true,
-        parse: value => path.normalize(value)
-    }
+	NODE_ENV: {
+		key: 'NODE_ENV',
+		name: 'environment',
+		desc: `{String} Current environment likely 'production' or 'development'.`,
+		default: 'development',
+		required: false,
+		parse: (value) => value.toString().toLowerCase(),
+	},
+	ROOT_DIR: {
+		key: 'ROOT_DIR',
+		name: 'rootDir',
+		desc: `{String} The root directory (absolute system path) for the application.`,
+		default: __dirname,
+		required: true,
+		parse: (value) => path.normalize(value),
+	},
 });
-
 
 module.exports = CONFIG_PROPERTIES;
