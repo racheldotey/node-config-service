4846f1fb29a8611fd2671cd93e8a8db26af4a4bc
Rachel Dotey <rachel.dotey@gmail.com>
2022-10-07 17:20:41 -0400

    Create jest.eslintrc.js

---
+++ config/eslint/lib/jest.eslintrc.js
@@ -0,0 +1,21 @@
+/**
+ * @requires `eslint-plugin-jest`
+ * @desc ESLint plugin for Jest.
+ * @link https://github.com/jest-community/eslint-plugin-jest
+ *
+ * @requires `eslint-plugin-jest-formatting`
+ * @desc ESLint rules for formatting test suites written for jest.
+ * @link https://github.com/dangreenisrael/eslint-plugin-jest-formatting
+ */
+module.exports = {
+	overrides: [
+		{
+			files: ['test/**'],
+			plugins: ['jest', 'jest-formatting'],
+			extends: ['plugin:jest/recommended', 'plugin:jest-formatting/recommended'],
+			rules: {
+				indent: ['error', 2],
+			},
+		},
+	],
+};
