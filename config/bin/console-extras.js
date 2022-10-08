35d796cff98f3e260871197342a7e88df4ea1eec
Rachel Dotey <rachel.dotey@gmail.com>
2022-10-07 23:18:33 -0400

    Create console-extras.js

---
+++ config/bin/console-extras.js
@@ -0,0 +1,52 @@
+var logPrefix;
+
+
+const init = prefix => {
+    exports.setPrefix(prefix);
+    return exports;
+}
+
+// Prepend log prefix log string
+const prefix = args => {
+    if (logPrefix) args.unshift(`${logPrefix} `);
+
+    return args;
+}
+
+const setPrefix = (msgPrefix = false) => {
+    logPrefix = (msgPrefix) ? `${msgPrefix}` : false;
+    return exports;
+}
+
+const debug = (...args) => {
+    console.debug.apply(console, prefix(args));
+}
+
+const error = (...args) => {
+    console.error.apply(console, prefix(args));
+}
+
+const info = (...args) => {
+    console.info.apply(console, prefix(args));
+}
+
+const log = (...args) => {
+    console.log.apply(console, prefix(args));
+}
+
+
+
+// We are a clone of `window.console`
+Object.assign(exports, console);
+exports.init = init;
+exports.setPrefix = setPrefix;
+exports.debug = debug;
+exports.error = error;
+exports.info = info;
+exports.log = log;
+
+
+
+(function constructor() {
+    logPrefix = false;
+})();
