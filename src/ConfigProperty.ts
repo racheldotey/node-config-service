d4020281d7b45e6c49473861b819983ac0a76eea
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-01 19:59:06 -0500

    Create ConfigProperty.ts

---
+++ src/ConfigProperty.ts
@@ -0,0 +1,91 @@
+type ConfigPropertyParseFunction = (value: string) => any;
+interface ConfigPropertyOptions {
+    envKey?: string;
+    desc?: string;
+    default?: string;
+    required?: boolean;
+    parse?: ConfigPropertyParseFunction;
+    value?: any;
+};
+
+/**
+ * A single property value for the config.
+ * @property {string} name - Key used to find the property within the program.
+ * @property {string | false} envKey - Key to system process env property.
+ * @property {string} desc - Description of this property used for logging.
+ * @property {string | null} default - Default value for this property
+ * @property {boolean} isDefined - Has this property been set/initialized.
+ * @property {boolean} isRequired - This must be set or it is invalid.
+ * @property {any} errors - Array of errors that have occurred with this property.
+ * @property {any} parse -  Parse the property (all process .env vars are strings by default)
+ * @property {any} #value - Property value
+ */
+class ConfigProperty {
+    name: string;
+    envKey: string | false;
+    desc: string;
+    default?: string;
+    isDefined: boolean;
+    isRequired: boolean;
+    errors?: Error[];
+    parse: ConfigPropertyParseFunction;
+    #value: any;
+
+    constructor(name: string, options: ConfigPropertyOptions = {}) {
+        this.name = name;
+        this.envKey = options.envKey || false;
+        this.desc = options.desc || '';
+        this.default = options.default || undefined;
+        this.isDefined = false;
+        this.isRequired = options.required || false;
+        this.parse = options.parse || (value => value);
+
+        if (this.default) {
+            this.#value = this.parse(this.default);
+        }
+
+        if (options.value) {
+            this.#value = options.value;
+            this.isDefined = true;
+        }
+    }
+
+    get value() {
+        if (!this.isDefined) throw new ReferenceError(`Config property "${this.name}" was requested before it was initialized.`);
+        return this.#value;
+    }
+
+    set value(envVars: { [key: string]: string }) {
+        this.setValue(envVars);
+    }
+
+    setValue(envVars: { [key: string]: string }) {
+        // Was the value already set, possibly at init
+        if (this.isDefined) return;
+
+        var value = this.default;
+
+        // Is the property set in the environment
+        if (this.envKey && envVars[this.envKey]) {
+            value = envVars[this.envKey];
+        } else if (envVars[this.name]) {
+            value = envVars[this.name];
+        }
+
+        if (value) {
+            this.#value = this.parse(value);
+            this.isDefined = true;
+        } else if (this.isRequired) {
+            throw new ReferenceError(`Config property "${this.name}" is required but not defined.`);
+        }
+    }
+
+    isMatch(find: string) {
+        if (!this.isDefined) throw new ReferenceError(`Config property "${this.name}" was requested before it was initialized.`);
+        return (find === this.name || find === this.envKey) ? true : false;
+    }
+};
+
+
+
+module.exports = exports = ConfigProperty;
