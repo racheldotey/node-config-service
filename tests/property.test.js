2e458e2c3ec026088bb31d249d724a420076b040
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-02 12:23:37 -0500

    Update property.test.js
    Filled out the unit tests for ConfigProperty

--- tests/property.test.js
+++ tests/property.test.js
@@ -1,96 +1,135 @@
 require('dotenv').config({ path: './test.basic.env' });
-const path = require('path');
 const ConfigProperty = require('../src/ConfigProperty');
 
 
-describe('TESTING CLASS `ConfigProperty`', () => {
+describe('> Test suite for class `ConfigProperty`:', () => {
     const key = 'bird';
     const options = {
         envKey: 'BIRD',
         desc: 'Have you herd the word?',
-        default: 'word',
-        required: true
+        default: 'word'
+    };
+    const data = {
+        type: 'Birthday cake',
+        flavor: 'Chocolate',
+        filling: 'Cherry',
+        layers: [1, 2, 3],
+        sing: () => console.log('HAPPY BIRTHDAY!')
     };
 
-    test('ConfigProperty Test - Empty options', () => {
+    test('(1) - Init with empty options.', () => {
         const prop = new ConfigProperty('empty_options');
-        console.debug('ConfigProperty Test - Empty options', '\n\r\n\r', prop);
+
         expect(prop.name).toMatch('empty_options');
         expect(prop.envKey).toBeFalsy();
         expect(prop.desc).toMatch('');
         expect(prop.default).toBeUndefined();
-        expect(prop.isRequired).toBeFalsy();
+        expect(prop.isRequired).toBeTruthy();
         expect(prop.isDefined).toBeFalsy();
-        expect(() => prop.value).toThrow();
-        expect(() => prop.isMatch()).toThrow();
     });
 
-    test('ConfigProperty Test - Simple options', () => {
-        const prop = new ConfigProperty(key, options);
-        console.debug('ConfigProperty Test - Simple options', '\n\r\n\r', prop);
+    test('(2) - Init with simple options.', () => {
+        var prop = new ConfigProperty(key, { ...options, required: false });
+
         expect(prop.name).toMatch(key);
         expect(prop.envKey).toMatch(options.envKey);
         expect(prop.desc).toMatch(options.desc);
         expect(prop.default).toEqual(options.default);
-        expect(prop.isRequired).toEqual(options.required);
+        expect(prop.isRequired).toBeFalsy();
+        expect(prop.isDefined).toBeFalsy();
+
+        prop = new ConfigProperty(key, { ...options, required: true });
+        expect(prop.isRequired).toBeTruthy();
+    });
+
+    test('(3) - Throw error if value is requested before being set.', () => {
+        const prop = new ConfigProperty(key);
+
         expect(prop.isDefined).toBeFalsy();
         expect(() => prop.value).toThrow();
+    });
+
+    test('(3) - Do not throw error when value is requested if value is set in the constructor.', () => {
+        const prop = new ConfigProperty(key, { ...options, value: key });
+
+        expect(prop.isDefined).toBeTruthy();
+        expect(prop.value).toMatch(key);
+    });
+
+    test('(4) - Throw error if isMatch() is called before value was set.', () => {
+        const prop = new ConfigProperty(key);
+
+        expect(prop.isDefined).toBeFalsy();
         expect(() => prop.isMatch()).toThrow();
     });
 
-    test('ConfigProperty Test - Force value', () => {
-        const value = { flavor: 'carrot', filling: 'cream cheese' };
-        const prop = new ConfigProperty(key, { ...options, value });
-        expect(prop.default).toEqual(options.default);
-        expect(prop.isDefined).toBeTruthy();
-        expect(JSON.stringify(prop.value)).toBe(JSON.stringify(value));
-        console.debug('ConfigProperty Test - Force value', '\n\r\n\r', {
-            name: prop.name,
-            default: prop.default,
-            isDefined: prop.isDefined,
-            value: prop.value
-        });
-    });
-
-    test('ConfigProperty Test - isMatch()', () => {
+    test('(5) - Test isMatch() method when value is set in the constructor.', () => {
         const value = { foo: 'bar' };
         const prop = new ConfigProperty(key, { ...options, value });
-        console.debug('ConfigProperty Test - isMatch()', '\n\r\n\r', {
-            name: prop.name,
-            envKey: prop.envKey
-        });
+
+        expect(prop.isDefined).toBeTruthy();
+        expect(prop.isMatch()).toBeFalsy();
         expect(prop.isMatch(options.envKey)).toBeTruthy();
         expect(prop.isMatch(key)).toBeTruthy();
         expect(prop.isMatch('dog')).toBeFalsy();
     });
 
-    test('ConfigProperty Test - Simple parse', () => {
-        const value = JSON.stringify({ flavor: 'chocolate', filling: 'cherry' });
-        const prop = new ConfigProperty(key, { ...options, default: value, parse: value => JSON.parse(value) });
-        prop.value = {};
-        console.debug('ConfigProperty Test - Force value', '\n\r\n\r', {
-            name: prop.name,
-            default: prop.default,
-            isDefined: prop.isDefined,
-            value: prop.value
-        });
+    test('(6) - Set advanced value in the constructor.', () => {
+        const prop = new ConfigProperty(key, { ...options, value: data });
+
+        expect(prop.isDefined).toBeTruthy();
+        expect(prop.isRequired).toBeTruthy();
+        expect(prop.default).toEqual(options.default);
+        expect(JSON.stringify(prop.value)).toBe(JSON.stringify(data));
     });
 
-    test('ConfigProperty Test - API_URL', () => {
-        const options = {
-            envKey: 'API_URL',
-            desc: 'Node.js process environment variable API_URL',
-            default: 'production',
-            required: true
-        };
-        const prop = new ConfigProperty('environment', options);
-        prop.value = process.env;
-        console.debug('ConfigProperty Test - API_URL', '\n\r\n\r', {
-            name: prop.name,
-            default: prop.default,
-            isDefined: prop.isDefined,
-            value: prop.value
-        });
+    test('(7) - Set value using default value and parse method.', () => {
+        const defaultValue = JSON.stringify(data);
+        const prop = new ConfigProperty(key, { ...options, default: defaultValue, parse: prop => JSON.parse(prop) });
+
+        expect(prop.isDefined).toBeFalsy();
+
+        prop.setValue({});
+
+        expect(prop.isDefined).toBeTruthy();
+        expect(prop.isRequired).toBeTruthy();
+        expect(prop.default).toEqual(defaultValue);
+        expect(JSON.stringify(prop.value)).toBe(defaultValue);
     });
 
+    test('(8) - Set value from env object. No parsing.', () => {
+        const prop = new ConfigProperty(key, options);
+
+        expect(prop.isDefined).toBeFalsy();
+
+        prop.setValue({ [options.envKey]: 'Parrots can talk.' });
+
+        expect(prop.isDefined).toBeTruthy();
+        expect(prop.isRequired).toBeTruthy();
+        expect(prop.default).toEqual(options.default);
+        expect(prop.value).toBe('Parrots can talk.');
+
+    });
+
+    test('(9) - Set value from env object with parsing.', () => {
+        const value = JSON.stringify(data);
+        const prop = new ConfigProperty(key, { envKey: options.envKey, parse: prop => JSON.parse(prop) });
+
+        expect(prop.isDefined).toBeFalsy();
+        expect(prop.isRequired).toBeTruthy();
+
+        prop.setValue({ [options.envKey]: value });
+
+        expect(prop.isDefined).toBeTruthy();
+        expect(JSON.stringify(prop.value)).toBe(value);
+    });
+
+    test('(10) - Throw error if setValue() cannot find its key when the property isRequired', () => {
+        const prop = new ConfigProperty(key, { envKey: options.envKey });
+
+        expect(prop.isDefined).toBeFalsy();
+        expect(prop.isRequired).toBeTruthy();
+
+        expect(() => prop.setValue({})).toThrow();
+    });
 });
