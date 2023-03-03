2a84ec93545ad7c8c7abb9cdb7e9f63468e1b034
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-02 23:35:38 -0500

    Update service.test.ts

--- tests/service.test.ts
+++ tests/service.test.ts
@@ -2,8 +2,24 @@ import ConfigService from '../src/ConfigService';
 
 
 describe('> Test suite for class `ConfigService`:', () => {
-    test('(1) - ', () => {
+    test('(1) - Verify interface', () => {
+        const config = new ConfigService();
+        // All props
+        expect(config.silenceErrors).toBeFalsy();
+        expect(config.logErrors).toBeFalsy();
+        expect(config.logFunction).toBeUndefined();
+        // All methods
+        expect(typeof config.init).toBe('function');
+        expect(typeof config.get).toBe('function');
+        expect(typeof config.findOne).toBe('function');
+        expect(typeof config.findSeveral).toBe('function');
+    });
 
+    test('(2) - Init with empty settings', () => {
+        const config = new ConfigService();
+        const result = config.init();
+
+        expect(result.constructor.name).toMatch('ConfigService');
     });
 });
 
