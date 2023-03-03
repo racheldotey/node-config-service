0451028c4d524a086aabfff0b4bdbccd4edfe28e
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-03 17:03:27 -0500

    Renamed the types file to main

---
+++ src/main.d.ts
@@ -0,0 +1,67 @@
+// Property Types
+export type ConfigPropertyParseFunction = (value: string) => any;
+
+export interface ConfigPropertyOptions {
+	envKey?: string;
+	desc?: string;
+	default?: string;
+	required?: boolean;
+	parse?: ConfigPropertyParseFunction;
+	value?: any;
+}
+
+// @see https://blog.logrocket.com/writing-constructor-typescript/
+export interface ConfigPropertyConstructor {
+	new (name: string, options?: ConfigPropertyOptions): IConfigProperty;
+}
+
+export interface IConfigProperty {
+	name: string;
+	envKey: string | false;
+	desc: string;
+	default?: string;
+	isDefined: boolean;
+	isRequired: boolean;
+	errors?: Error[];
+	parse: ConfigPropertyParseFunction;
+	get value(): {
+		[key: string]: string;
+	};
+	set value(envVars: NodeJS.ProcessEnv);
+	setValue(envVars: NodeJS.ProcessEnv): void;
+	isMatch(find: string): boolean;
+}
+
+// Service Types
+export type ConfigServiceLogFunction = (...data: any[]) => void;
+
+export interface DefinePropertyOptions {
+	[key: string]: ConfigPropertyOptions;
+};
+
+export interface ConfigServiceOptions {
+	silenceErrors?: boolean;
+	logErrors?: boolean;
+	logFunction?: ConfigServiceLogFunction;
+	properties?: DefinePropertyOptions;
+}
+
+// @see https://blog.logrocket.com/writing-constructor-typescript/
+export interface ConfigServiceConstructor {
+	new(options?: ConfigServiceOptions): IConfigService;
+}
+
+export interface IConfigService {
+	silenceErrors?: boolean;
+	logErrors?: boolean;
+	logFunction?: ConfigServiceLogFunction;
+	init(processEnv?: NodeJS.ProcessEnv): IConfigService;
+	get properties(): {
+		[key: string]: IConfigProperty;
+	};
+	get(...args: string[]): any;
+	findOne(find: string): any;
+	findSeveral(names: string[]): {
+		[k: string]: any;
+	};
+}
