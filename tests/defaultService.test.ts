081fc6c430f12d24b950b40017ec0d113ba58088
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-03 17:06:05 -0500

    Create defaultService.test.ts

---
+++ tests/defaultService.test.ts
@@ -0,0 +1,13 @@
+import getDefaultService from '../src/getDefaultService';
+
+describe('> Test suite for function `getDefaultService`:', () => {
+    var config = getDefaultService();
+
+    test('(1) - Verify the interface.', () => {
+        expect(config).toBeDefined();
+        expect(config.constructor.name).toMatch('ConfigService');
+    });
+
+});
+
+export { };
