66df86b6843cb3eb56452f87b949861cfe376c33
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-02 23:35:09 -0500

    Update ConfigService.ts

--- src/ConfigService.ts
+++ src/ConfigService.ts
@@ -1,20 +1,98 @@
-import { ConfigServiceOptions, IConfigProperty } from './types';
-import ConfigProperty from './ConfigProperty';
+import { ConfigProperty, IConfigProperty, ConfigPropertyOptions } from './ConfigProperty';
 
-export class ConfigService {
-	options: ConfigServiceOptions;
-	properties: Array<ConfigProperty>;
 
-	constructor(options: ConfigServiceOptions = {}) {
-		this.options = {
-			silenceErrors: false,
-			logErrors: false,
-			logFunction: console.log,
-			...options,
-		};
+export type ConfigServiceLogFunction = (value: any) => void;
 
-        this.properties = [];
-	}
+export interface DefinePropertyOptions {
+	[key: string]: ConfigPropertyOptions;
+};
+
+export interface ConfigServiceOptions {
+	silenceErrors?: boolean;
+	logErrors?: boolean;
+	logFunction?: ConfigServiceLogFunction;
+	properties: DefinePropertyOptions;
 }
+
+// @see https://blog.logrocket.com/writing-constructor-typescript/
+export interface ConfigServiceConstructor {
+	new(options?: ConfigServiceOptions): IConfigService;
+}
+
+export interface IConfigService {
+	silenceErrors?: boolean;
+	logErrors?: boolean;
+	logFunction?: ConfigServiceLogFunction;
+	init(processEnv?: NodeJS.ProcessEnv): IConfigService;
+	get properties(): {
+		[key: string]: IConfigProperty;
+	};
+	get(...args: string[]): any;
+	findOne(find: string): any;
+	findSeveral(names: string[]): {
+		[k: string]: any;
+	};
+}
+
+export const ConfigService: ConfigServiceConstructor = class ConfigService implements IConfigService {
+	silenceErrors?: boolean;
+	logErrors?: boolean;
+	logFunction?: ConfigServiceLogFunction;
+	#properties: {
+		[key: string]: IConfigProperty;
+	};
+
+	constructor(options: ConfigServiceOptions = { properties: {} }) {
+		this.silenceErrors = (options.silenceErrors) ? true : false;
+		this.logErrors = (options.logErrors) ? true : false;
+		this.logFunction = (options.logFunction) ? options.logFunction : undefined;
+
+		this.#properties = {};
+		Object.entries(options.properties).forEach(([name, options]) => {
+			const prop = new ConfigProperty(name, options);
+			this.#properties[prop.name] = prop;
+		});
+	}
+
+	init(processEnv: NodeJS.ProcessEnv = process.env) {
+		try {
+			Object.values(this.#properties).forEach(prop => prop.setValue(processEnv));
+			return this;
+		} catch (error) {
+			throw new Error('Could not set app config properties. Invalid process env sent ConfigService#init.');
+		};
+	}
+
+	get properties() {
+		// Will throw error if properties were never set
+		if (this.#properties) return this.#properties;
+		throw new Error('app config properties requested before initialization.');
+	}
+
+	get(...args: string[]) {
+		if(!args.length) return this.findSeveral(Object.keys(this.#properties));
+
+		if(args.length === 1) {
+			return (Array.isArray(args[0])) ? this.findSeveral(args[0]) : this.findOne(args[0]);
+		}
+
+		return this.findSeveral(args);
+	}
+
+	findOne(find: string) {
+		return this.#properties?.[find] || Object.values(this.#properties).find(prop => prop.isMatch(find));
+	}
+
+	findSeveral(names: string[]) {
+		return Object.fromEntries(
+			names.map((name) => {
+				const prop = this.findOne(name);
+				return [prop.name, prop.value];
+			})
+		);
+	}
+};
+
+
 
 export default ConfigService;
