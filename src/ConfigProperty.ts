5bc87e1ae08b2c02dea2f47b97961ea362a23a07
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-03 14:36:17 -0500

    Update ConfigProperty.ts

--- src/ConfigProperty.ts
+++ src/ConfigProperty.ts
@@ -1,34 +1,34 @@
 export type ConfigPropertyParseFunction = (value: string) => any;
 
 export interface ConfigPropertyOptions {
-    envKey?: string;
-    desc?: string;
-    default?: string;
-    required?: boolean;
-    parse?: ConfigPropertyParseFunction;
-    value?: any;
+	envKey?: string;
+	desc?: string;
+	default?: string;
+	required?: boolean;
+	parse?: ConfigPropertyParseFunction;
+	value?: any;
 }
 
 // @see https://blog.logrocket.com/writing-constructor-typescript/
 export interface ConfigPropertyConstructor {
-    new (name: string, options?: ConfigPropertyOptions): IConfigProperty;
+	new (name: string, options?: ConfigPropertyOptions): IConfigProperty;
 }
 
 export interface IConfigProperty {
-    name: string;
-    envKey: string | false;
-    desc: string;
-    default?: string;
-    isDefined: boolean;
-    isRequired: boolean;
-    errors?: Error[];
-    parse: ConfigPropertyParseFunction;
-    get value(): {
-        [key: string]: string;
-    };
-    set value(envVars: NodeJS.ProcessEnv);
-    setValue(envVars: NodeJS.ProcessEnv): void;
-    isMatch(find: string): boolean;
+	name: string;
+	envKey: string | false;
+	desc: string;
+	default?: string;
+	isDefined: boolean;
+	isRequired: boolean;
+	errors?: Error[];
+	parse: ConfigPropertyParseFunction;
+	get value(): {
+		[key: string]: string;
+	};
+	set value(envVars: NodeJS.ProcessEnv);
+	setValue(envVars: NodeJS.ProcessEnv): void;
+	isMatch(find: string): boolean;
 }
 
 /**
@@ -43,7 +43,9 @@ export interface IConfigProperty {
  * @property {any} parse -  Parse the property (all process .env vars are strings by default)
  * @property {any} #value - Property value
  */
-export const ConfigProperty: ConfigPropertyConstructor = class ConfigProperty implements IConfigProperty {
+export const ConfigProperty: ConfigPropertyConstructor = class ConfigProperty
+	implements IConfigProperty
+{
 	name: string;
 	envKey: string | false;
 	desc: string;
@@ -80,10 +82,7 @@ export const ConfigProperty: ConfigPropertyConstructor = class ConfigProperty im
 	}
 
 	get value() {
-		if (!this.isDefined)
-			throw new ReferenceError(
-				`Config property "${this.name}" was requested before it was initialized.`
-			);
+		if (!this.isDefined) throw new ReferenceError(`Config property "${this.name}" was requested before it was initialized.`);
 		return this.#value;
 	}
 
@@ -119,7 +118,6 @@ export const ConfigProperty: ConfigPropertyConstructor = class ConfigProperty im
 			);
 		return find === this.name || find === this.envKey ? true : false;
 	}
-}
-
+};
 
 export default ConfigProperty;
