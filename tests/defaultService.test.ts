e534553df12d16f2e5d0f732e6801d53ebe01b16
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-07 09:40:50 -0500

    Update defaultService.test.ts

--- tests/defaultService.test.ts
+++ tests/defaultService.test.ts
@@ -1,11 +1,11 @@
+import { expectConfigServiceInterface } from './lib/expectConfigServiceInterface';
 import { getDefaultService } from '../src/getDefaultService';
 
 describe('> Test suite for function `getDefaultService`:', () => {
     var config = getDefaultService();
 
     test('(1) - Verify the interface.', () => {
-        expect(config).toBeDefined();
-        expect(config.constructor.name).toMatch('ConfigService');
+        expectConfigServiceInterface(config);
     });
 
 });
