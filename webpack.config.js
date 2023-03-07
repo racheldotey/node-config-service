bf20d19508f23be034389246ac09b6871fb95992
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-07 09:40:25 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const production = process.env.NODE_ENV === 'production' || false;
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
+	entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+		fallback: {
+			"fs": false,
+			"os": false,
+			"path": false
+		},
+    },
+    mode: 'production',
+    output: {
+        filename: production ? 'node-config-service.min.js' : 'node-config-service.js',
+        path: path.resolve(__dirname, 'dist'),
+        globalObject: 'this',
+        library: 'DisableWith',
+        libraryExport: 'default',
+        libraryTarget: 'umd'
+    },
+    optimization: {
+        minimize: production,
+        minimizer: [
+            new TerserPlugin({})
+        ]
+    },
+	plugins: [
+	  new Dotenv()
+	],
 };
