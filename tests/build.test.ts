b9e8772362d1f64db6f590e3ac0c44a776380a9f
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-07 12:28:19 -0500

    Update build.test.ts

--- tests/build.test.ts
+++ tests/build.test.ts
@@ -1,10 +1,15 @@
 import { expectConfigServiceInterface } from './lib/expectConfigServiceInterface';
-import config from '../dist';
+import config from '../dist/node-config-service.js';
 
 describe('Test the current `dist` build', () => {
 
+    console.log(JSON.parse(JSON.stringify(config)));
+
     test('(1) - Confirm expected module interface.', () => {
         expectConfigServiceInterface(config);
+    });
+
+    test('(2) - Confirm empty properties.', () => {
     });
 });
 
