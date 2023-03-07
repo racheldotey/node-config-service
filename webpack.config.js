bff2cee9584a16b3e8ae608bc0504e39ceaadcc3
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-07 14:43:43 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,95 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const Dotenv = require('dotenv-webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 
-const production = process.env.NODE_ENV === 'production' || false;
+const TS_CONFIG = path.resolve(__dirname, 'tsconfig.json');
+const OUTPUT_DIR = path.resolve(__dirname, 'dist');
 
-module.exports = {
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    mode: 'production',
-    output: {
-        filename: production ? 'node-config-service.min.js' : 'node-config-service.js',
-        path: path.resolve(__dirname, 'dist'),
-        globalObject: 'this',
-        library: 'DisableWith',
-        libraryExport: 'default',
-        libraryTarget: 'umd'
-    },
-    optimization: {
-        minimize: production,
-        minimizer: [
-            new TerserPlugin({})
-        ]
-    },
-    plugins: [
-        new Dotenv()
-    ],
-    resolve: {
-        extensions: ['.ts', '.js'],
-        fallback: {
-            "fs": false,
-            "os": false,
-            "path": false
+const extractMode = (env, argv, defaultMode = 'development') => {
+    var mode = defaultMode;
+    if (argv.mode || env.mode) {
+        mode = argv.mode ?? env.mode;
+    } else {
+        dotenv.config();
+        mode = process.env.NODE_ENV ?? defaultMode;
+    }
+    if (!['development', 'production'].includes(mode)) mode = defaultMode;
+
+    return mode;
+};
+
+module.exports = (env, argv) => {
+    const mode = extractMode(env, argv);
+    const isProduction = mode === 'production' ? true : false;
+
+    console.info(`\n\r[node-config-service:build] Running webpack in '${mode}' mode.\n\r`);
+
+    const config = {
+        devtool: 'inline-source-map',
+        entry: {
+            'node-config-service': ['./src/index.ts'],
         },
-    },
+        module: {
+            rules: [
+                {
+                    // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+                    // @see https://github.com/TypeStrong/ts-loader
+                    test: /\.([cm]?ts|tsx)$/,
+                    //use: 'ts-loader',
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                    options: { configFile: TS_CONFIG },
+                },
+            ],
+        },
+        mode: mode,
+        output: {
+            filename: isProduction ? `[name].min.js` : `[name].js`,
+            path: OUTPUT_DIR,
+            globalObject: 'this',
+            umdNamedDefine: true,
+            library: {
+                // all options under `output.library` can be used here
+                name: 'NodeConfigService',
+                type: 'umd',
+                umdNamedDefine: true,
+                //export: ['default', 'ConfigService'],
+                auxiliaryComment: `[npm:node-config-service] Copyright(c) 2021-present Rachel Dotey <https://github.com/racheldotey/node-config-service> MIT Licensed`,
+            },
+        },
+        optimization: {
+            minimize: isProduction,
+            minimizer: [new TerserPlugin({})],
+        },
+        plugins: [
+            new Dotenv(),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(mode),
+            }),
+        ],
+        resolve: {
+            // Add `.ts` and `.tsx` as a resolvable extension.
+            extensions: ['.ts', '.tsx', '.js'],
+            // Add support for TypeScripts fully qualified ESM imports.
+            extensionAlias: {
+                '.js': ['.js', '.ts'],
+                '.cjs': ['.cjs', '.cts'],
+                '.mjs': ['.mjs', '.mts'],
+            },
+            fallback: {
+                fs: false,
+                os: false,
+                path: false,
+            },
+        },
+    };
+
+    // @see https://webpack.js.org/configuration/devtool/
+    /* if (!isProduction) {
+        config.devtool = 'inline-source-map';
+    } */
+
+    return config;
 };
