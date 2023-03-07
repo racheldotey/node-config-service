071afe7a295ede674af767e36c57b31fdb31469f
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-07 09:40:44 -0500

    Create expectConfigServiceInterface.ts

---
+++ tests/lib/expectConfigServiceInterface.ts
@@ -0,0 +1,15 @@
+import { IConfigService } from '../../src/main';
+
+export const expectConfigServiceInterface = (config: IConfigService) => {
+	expect(config).toBeDefined();
+	expect(config.constructor.name).toMatch('ConfigService');
+	// All props
+	expect(config).toHaveProperty('silenceErrors');
+	expect(config).toHaveProperty('logErrors');
+	expect(config).toHaveProperty('logFunction');
+	// All methods
+	expect(typeof config.init).toBe('function');
+	expect(typeof config.get).toBe('function');
+	expect(typeof config.findOne).toBe('function');
+	expect(typeof config.findSeveral).toBe('function');
+};
