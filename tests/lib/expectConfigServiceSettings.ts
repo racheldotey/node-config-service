dca8d4a9728b54867faebefa0985f397ab19ed4c
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-07 09:40:47 -0500

    Create expectConfigServiceSettings.ts

---
+++ tests/lib/expectConfigServiceSettings.ts
@@ -0,0 +1,28 @@
+import { IConfigService, ConfigServiceLogFunction } from '../../src/main';
+
+export const expectConfigServiceSettings = (
+    config: IConfigService,
+    {
+        silenceErrors,
+        logErrors,
+        logFunction,
+    }: { silenceErrors?: boolean; logErrors?: boolean; logFunction?: ConfigServiceLogFunction } = {}
+) => {
+    if (silenceErrors) {
+        expect(config.silenceErrors).toBeTruthy();
+    } else {
+        expect(config.silenceErrors).toBeFalsy();
+    }
+
+    if (logErrors) {
+        expect(config.logErrors).toBeTruthy();
+    } else {
+        expect(config.logErrors).toBeFalsy();
+    }
+
+    if (logFunction) {
+        expect(typeof config.logFunction).toBe('function');
+    } else {
+        expect(config.logFunction).toBeUndefined();
+    }
+};
