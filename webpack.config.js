a353952586506cf7d13926132f2bce4fab17f8df
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-08 14:05:16 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,21 @@ const dotenv = require('dotenv');
 const Dotenv = require('dotenv-webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 
-const TS_CONFIG = path.resolve(__dirname, 'tsconfig.json');
-const OUTPUT_DIR = path.resolve(__dirname, 'dist');
-const auxiliaryComment = `[npm:node-config-service] Copyright(c) 2021-${new Date().getFullYear()} Rachel Dotey <https://github.com/racheldotey/node-config-service> MIT Licensed`;
+
+const packageName = 'node-config-service';
+const packageVersion = '0.9.1';
+
+const pathRoot = path.resolve(__dirname);
+const pathDist = path.resolve(pathRoot, 'dist');
+const pathTsConfig = path.resolve(pathRoot, 'tsconfig.json');
+const dateBuilt = new Date();
+const auxiliaryComment = `Node-Config-Service v${packageVersion} Copyright(c) 2021-${dateBuilt.getFullYear()} Rachel Dotey <https://github.com/racheldotey/node-config-service> MIT Licensed`;
 
 
-
-const extractMode = (env, argv, defaultMode = 'development') => {
+const extractMode = (env = {}, argv = {}, defaultMode = 'production') => {
 	var mode = defaultMode;
-	if (argv.mode || env.mode) {
-		mode = argv.mode ?? env.mode;
+	if (argv.mode || argv.nodeEnv || env.mode || env.nodeEnv) {
+		mode = argv.mode ?? argv.nodeEnv ?? env.mode ?? env.nodeEnv;
 	} else {
 		dotenv.config();
 		mode = process.env.NODE_ENV ?? defaultMode;
@@ -22,80 +27,65 @@ const extractMode = (env, argv, defaultMode = 'development') => {
 	return mode;
 };
 
-
-
-const config = {
-	entry: {
-		'node-config-service': './src/index.ts'
-	},
-	module: {
-		rules: [
-			{
-				// all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
-				// @see https://github.com/TypeStrong/ts-loader
-				test: /\.([cm]?ts|tsx)$/,
-				loader: 'ts-loader',
-				exclude: ['/node_modules/'],
-				options: { configFile: TS_CONFIG },
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-				type: 'asset',
-			},
-
-			// Add your rules for custom modules here
-			// Learn more about loaders from https://webpack.js.org/loaders/
-		],
-	},
-	optimization: {
-		minimize: false,
-		minimizer: [new TerserPlugin({})],
-	},
-	output: {
-		clean: false,
-		filename: `[name].js`,
-		globalObject: 'this',
-		library: {
-			name: 'NodeConfigService',
-			auxiliaryComment,
-			//export: ['default','NodeConfigService'],
-			type: 'umd',
-			umdNamedDefine: true,
-		},
-		path: OUTPUT_DIR,
-	},
-	plugins: [
-		// Add your plugins here
-		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
-		new Dotenv({
-			silent: false
-		}),
-	],
-	resolve: {
-		// Add `.ts` and `.tsx` as a resolvable extension.
-		extensions: ['.ts', '.tsx', '.js'],
-		// Add support for TypeScripts fully qualified ESM imports.
-		extensionAlias: {
-			'.js': ['.js', '.ts'],
-			'.cjs': ['.cjs', '.cts'],
-			'.mjs': ['.mjs', '.mts'],
-		},
-		fallback: {
-			fs: false,
-			os: false,
-			path: false,
-		},
-	},
-};
-
 module.exports = (env, argv) => {
 	const mode = extractMode(env, argv);
-	const isProduction = mode === 'production' ? true : false;
 
-	console.info(`\n\r[node-config-service:build] Running webpack in '${mode}' mode.\n\r`);
+    const config = {
+        mode,
+        entry: {
+            'node-config-service': './src/index.ts'
+        },
+        module: {
+            rules: [
+                {
+                    // files with `.ts`, `.cts`, `.mts` or `.tsx` extensions will be handled by `ts-loader`
+                    // @see https://github.com/TypeStrong/ts-loader
+                    test: /\.([cm]?ts|tsx)$/,
+                    loader: 'ts-loader',
+                    exclude: ['/node_modules/'],
+                    options: { configFile: pathTsConfig },
+                },
+            ],
+        },
+        optimization: {
+            minimize: false,
+            minimizer: [new TerserPlugin({})],
+        },
+        output: {
+            filename: `[name].js`,
+            globalObject: 'this',
+            library: {
+                name: 'nodeConfigService',
+                auxiliaryComment,
+                //export: ['default','NodeConfigService'],
+                type: 'umd',
+                umdNamedDefine: true,
+            },
+            path: pathDist,
+        },
+        plugins: [
+            // @see https://webpack.js.org/configuration/plugins/
+            new Dotenv({
+                silent: false
+            }),
+        ],
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+            // Add support for TypeScripts fully qualified ESM imports.
+            extensionAlias: {
+                '.js': ['.js', '.ts'],
+                '.cjs': ['.cjs', '.cts'],
+                '.mjs': ['.mjs', '.mts'],
+            },
+            fallback: {
+                fs: false,
+                os: false,
+                path: false,
+            },
+        },
+    };
 
-	if (isProduction) {
-		config.mode = 'production';
+	if (mode === 'production') {
 		config.optimization.minimize = true;
 		config.output.filename = '[name].min.js';
 	} else {
@@ -105,6 +95,8 @@ module.exports = (env, argv) => {
 			host: 'localhost',
 		};
 	}
+
+	console.info(`\n\r[${packageName}:webpack] Running in '${config.mode}' mode.\n\r`);
 
 	return config;
 };
