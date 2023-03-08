69c8c11e00a6ccfc6dc2819d43ef5a9cafa994ac
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-08 14:05:09 -0500

    Build script

---
+++ bin/build-dist.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const webpack = require('webpack');
+
+const tinyLogger = require('./tinyLogger');
+const getWebpackConfig = require('../webpack.config');
+
+const packageName = 'node-config-service';
+const packageVersion = '0.9.1';
+
+const tl = tinyLogger(`  [${packageName}:build-dist]  `);
+tl.info(`Starting dist build of ${packageName} v${packageVersion}...`);
+
+const deleteDistFolder = ({ output }) => {
+    // Remove dist directory to start fresh
+    if (fs.existsSync(output.path)) {
+        tl.info(`Path ${output.path} already exists. Deleting old dist.`);
+        return fs.rmSync(output.path, { recursive: true });
+    }
+
+    return true;
+};
+
+const runWebpackBuild = async webpackConfig =>
+    new Promise((resolve, reject) => {
+        tl.info(`Building ${webpackConfig.mode} with webpack.`);
+        webpack(webpackConfig, (error, stats) => {
+            if (error || stats.hasErrors()) {
+                tl.error(error);
+                exit(1);
+                reject(error);
+            }
+            tl.info(`Build ${webpackConfig.mode} complete.`);
+            resolve(webpackConfig.mode);
+        });
+    });
+
+const getConfig = mode => getWebpackConfig({ mode });
+
+const main = async () => {
+    // Get webpack configs for each env mode
+    const development = getConfig('development');
+    const production = getConfig('production');
+
+    deleteDistFolder(development);
+    deleteDistFolder(production);
+
+    await runWebpackBuild(development);
+    await runWebpackBuild(production);
+};
+
+main();
