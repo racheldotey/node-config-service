3a476a26d1724fa16d42abf72b81e6191983cf5c
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-07 21:05:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 /**
+ * NodeConfigService
+ *
  * A Node.js app config management service for use in browser and on the server.
+ *
+ * Simplified config management for node applications.
+ * ConfigService accepts a configuration object of properties
+ * to be defined using the node process env and run time overrides.
  *
  * @version 0.9.0
  * @module node-config-service
