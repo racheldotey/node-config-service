918a0ce88c7f60da44fb568d72a474a4c44eeb38
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-08 14:04:07 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-/**
- * ConfigService
- *
- * A Node.js app config management service for use in browser and on the server.
- *
- * Simplified config management for node applications.
- * ConfigManager accepts a configuration object of properties
- * to be defined using the node process env and run time overrides.
- *
- * @version 0.9.0
- * @module node-config-service
- * @author Rachel Dotey <hello@racheldotey.com>
- */
-
 /** Exports the Node-Config-Service entry point. */
 import { ConfigProperty } from './ConfigProperty';
 import { ConfigManager } from './ConfigManager';
