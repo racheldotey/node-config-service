b1ce5ad228876adbc7771c91241a50788e802603
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-08 09:38:27 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * NodeConfigService
+ *
+ * A Node.js app config management service for use in browser and on the server.
+ *
+ * Simplified config management for node applications.
+ * ConfigService accepts a configuration object of properties
+ * to be defined using the node process env and run time overrides.
+ *
+ * @version 0.9.0
+ * @module node-config-service
+ * @author Rachel Dotey <hello@racheldotey.com>
+ */
+const NodeConfigService = require('./dist/node-config-service');
+
+/** Exports the Node-Config-Service entry point. */
+module.exports = NodeConfigService;
+module.exports.default = NodeConfigService;
+module.exports.NodeConfigService = NodeConfigService;
