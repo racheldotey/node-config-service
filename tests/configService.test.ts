9ddcda9536c3cd7d779101fe6aa5bc512709d36c
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-09 09:38:32 -0500

    Create configService.test.ts

---
+++ tests/configService.test.ts
@@ -0,0 +1,10 @@
+import { ConfigService } from '../src/ConfigService';
+
+
+describe('> Test suite for class `ConfigService`:', () => {
+    test('(1) - Placeholder', () => {
+        console.debug(ConfigService);
+    });
+});
+
+module.exports = {};
