bb0681bdc3c8988956d58041512b39cb2b2f1c16
Rachel Dotey <rachel.dotey@gmail.com>
2023-03-20 22:34:08 -0400

    Remove old structure and testing.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const { NodeConfigService, ConfigPropertyManager, ConfigProperty } = require('./main');
-
-module.exports = NodeConfigService;
-module.exports.default = NodeConfigService;
-module.exports.NodeConfigService = NodeConfigService;
-module.exports.ConfigPropertyManager = ConfigPropertyManager;
-module.exports.ConfigProperty = ConfigProperty;
